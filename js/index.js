56e9a0907c1d33a660363de7ab75fc0b20feb473
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2021-03-15 14:55:50 +0700

    Add css and javascript file, And edit file index.html

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log('Hello');
+$(window).scroll(function () {
+	let offsetTop = $(this).scrollTop();
+	// console.log(offsetTop);
+	if (offsetTop > 450) {
+		$('#header .navbar').addClass('navbar-scrolled');
+	} else {
+		$('#header .navbar').removeClass('navbar-scrolled');
+	}
+})
